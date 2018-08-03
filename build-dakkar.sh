74d0929f0abb94d11503cad044cc49a9715548e8
abun880007 <abun880007@msn.com>
2018-08-03 01:12:09 +0100

    fix dakkar build

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -243,7 +243,7 @@ function clone_or_checkout() {
             git checkout origin/"$localManifestBranch"
         )
     else
-        git clone https://github.com/phhusson/"$repo" "$dir" -b "$localManifestBranch"
+        git clone https://github.com/Team-UB/"$repo" "$dir" -b "$localManifestBranch"
     fi
 }
 
